71d7e8a410b50e83a73f5dfc12980f8978b6f09e
@rikatussei <yohei.arisu@Gmail.com>
2024-06-06 11:10:25 +0900

    Create 問題３.rb

---
+++ 問題３.rb
@@ -0,0 +1,26 @@
+# 問題.1
+class Article
+
+  def initialize(author, title, content)
+    @author = author
+    @title = title
+    @content = content
+  end
+
+end
+# 上記のコードに追加を行い、以下の出力結果を得られるようにしてください。ただし、クラスとインスタンスを使用するものとします。
+
+# 著者: 阿部
+# タイトル: Rubyの素晴らしさについて
+# 本文: Awesome Ruby!
+
+
+# 回答
+
+
+
+
+
+
+
+
